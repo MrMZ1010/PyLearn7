95001e1ea06ed4c500723c5409d9c5e9f4c6a64e
MohammadAli Mirzaei <mohammadalimirzaei.business@gmail.com>
2024-05-22 19:36:30 +0330

    bugs fixed

--- Basic/Assignment 12/actor.py
+++
@@ -1,7 +0,0 @@
-##### Mohammad Ali Mirzaei #####
-
-class Actor():
-    def __init__(self, actor_name):
-        self.actor_name = actor_name
-    
-    
--- Basic/Assignment 12/clip.py
+++
@@ -1,7 +0,0 @@
-##### Mohammad Ali Mirzaei #####
-
-from media import Media
-
-class Clip(Media):
-    def __init__(self, type, name, director, imdb_score, url, duration, casts):
-        super().__init__(type, name, director, imdb_score, url, duration, casts)
